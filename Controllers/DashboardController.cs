02c8d7874c76523b3a5da0195756740ffe660cc8
nitzan-nagar <nitzan.nagar@gmail.com>
2025-08-18 22:22:06 +0300

    test

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -57,7 +57,7 @@ namespace AI.CryptoAdvisor.Api.Controllers
                         .GetProperty("message")
                         .GetProperty("content")
                         .GetString();
-                    Console.WriteLine(insightText);
+                    Console.WriteLine("&&&&&&&&&&&&&&&&&&&&" + insightText);
                     return insightText;
                 },
                 cacheMinutes: 1440,
@@ -70,7 +70,7 @@ namespace AI.CryptoAdvisor.Api.Controllers
                 {
                     var url = "https://www.reddit.com/r/cryptomemes/.json?limit=100&t=day";
                     client.DefaultRequestHeaders.UserAgent.ParseAdd("CryptoDashboard/0.1");
-                    Console.WriteLine(await client.GetStringAsync(url));
+                    Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&" + await client.GetStringAsync(url));
                     return await client.GetStringAsync(url);
                 },
                 cacheMinutes: 5,
