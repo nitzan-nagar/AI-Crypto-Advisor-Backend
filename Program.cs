a4362eb56cc4f126ee4d3ecb136e36039eaf7b84
nitzan-nagar <nitzan.nagar@gmail.com>
2025-08-18 18:20:04 +0300

    migrations fix

--- Program.cs
+++ Program.cs
@@ -102,8 +102,12 @@ namespace AI.CryptoAdvisor.Api
 
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate(); 
+            }
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
